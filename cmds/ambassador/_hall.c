37441c3177fe29d40ca2155347e70ea625a23aa7
Michael Programs <michael.programs@gmail.com>
2019-12-31 20:57:05 -0500

    update home command and add hall command

--- cmds/ambassador/_hall.c
+++ cmds/ambassador/_hall.c
@@ -1,28 +1,29 @@
 #include <std.h>
 #include <move.h>
+#include <rooms.h>
 
 inherit DAEMON;
 
-int cmd_home(string str) {
-    object tp = this_player(), workroom, prev;
+int cmd_hall(string str) {
+    object tp = this_player(), room, prev;
     string dest;
 
     if(!creatorp(tp)) return 0;
 
-    dest = user_path( str ? lower_case(str) : tp->query_name() ) + "workroom";
+    dest = ROOM_ADMIN;
     prev = environment(tp);
 
-    if(!(workroom = find_object(dest))) workroom = load_object(dest);
+    if(!(room = find_object(dest))) room = load_object(dest);
 
-    if(prev == workroom) return notify_fail("You twitch.\n");
-    else if(tp->move(workroom) == MOVE_OK) {
+    if(prev == room) return notify_fail("You twitch.\n");
+    else if(tp->move(room) == MOVE_OK) {
         tp->describe_current_room(1);
         if(!hiddenp(tp)) {
             foreach(object ob in prev->query_living_contents()) {
                 if(ob == this_player() || (tp->query_invis() && !creatorp(ob))) continue;
                 message("mmout", (tp->query_invis() && creatorp(ob) ? "(invis) " : "")+strip_colours(tp->query_mhome(to_int(creatorp(ob)))), ob);
             }
-            foreach(object ob in workroom->query_living_contents()) {
+            foreach(object ob in room->query_living_contents()) {
                 if(ob == this_player() || (tp->query_invis() && !creatorp(ob))) continue;
                 message("mmin", (tp->query_invis() && creatorp(ob) ? "(invis) " : "")+strip_colours(tp->query_mmin(to_int(creatorp(ob)))), ob);
             }
