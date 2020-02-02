2307c67a15e1f807d763c6855f0164628f041535
Michael Programs <michael.programs@gmail.com>
2020-02-01 23:22:04 -0500

    update callouts command to take a filter

--- cmds/creator/_callouts.c
+++ cmds/creator/_callouts.c
@@ -3,26 +3,31 @@
 inherit DAEMON;
 
 int myfun(mixed a, mixed b) {
-if(a[2]==b[2]) return 0;
-  if (a[2]>b[2]) return 1;
-  return -1;
+    if(a[2]==b[2]) return 0;
+    if (a[2]>b[2]) return 1;
+    return -1;
 }
-mixed cmd_callouts(string args) {
+
+mixed cmd_callouts(string filter) {
     mixed *coi;
     string tmp;
     int i, maxi;
 
-    coi = call_out_info();
-coi = sort_array(coi, "myfun");
-    if( !sizeof(coi) ) {
+    if(filter && filter != "") {
+        coi = ({});
+        foreach(mixed m in call_out_info()) {
+            if(strsrch(identify(m[0]), filter) > -1) coi += ({m});
+        }
+    } else coi = call_out_info();
+
+    coi = sort_array(coi, "myfun");
+    if(!sizeof(coi)) {
         message("system", "No pending callouts.", this_player());
         return 1;
     }
-    tmp = sprintf("%:-40s %:-25s %:-6s\n", "Object", "Function",
-                  "Delay");
+    tmp = sprintf("%:-40s %:-25s %:-6s\n", "Object", "Function", "Delay");
 
-    tmp += "--------------------------------------------------"
-      "-------------------------\n";
+    tmp += "---------------------------------------------------------------------------\n";
     for(i=0, maxi = sizeof(coi); i<maxi; i++) {
         if( sizeof(coi[i]) != 3 ) {
             tmp += sprintf("Error in element %O\n", coi[i]);
@@ -37,7 +42,7 @@ coi = sort_array(coi, "myfun");
 }
 
 void help() {
-    message("help", "Syntax: <callouts>\n\n"
-            "Lists all pending callouts.\n\n"
+    message("help", "Syntax: <callouts (filter)>\n\n"
+            "Lists all pending callouts. Optional filter can be used display specific information.\n\n"
             "See also: events, mstatus, netstat", this_player());
 }
