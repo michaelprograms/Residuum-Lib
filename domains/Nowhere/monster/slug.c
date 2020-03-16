bfc0400703fe2dd63686425cfd9b5f69da6ba0c0
Michael Programs <michael.programs@gmail.com>
2020-03-15 21:30:50 -0400

    add a slug race and monster

---
+++ domains/Nowhere/monster/slug.c
@@ -0,0 +1,11 @@
+inherit MONSTER;
+
+void create() {
+    ::create();
+    set_name("slug");
+    set_short("slug");
+    set_long("A slug.");
+    set_id(({"slug"}));
+    set_race("slug");
+    set_body_type("slug");
+}
